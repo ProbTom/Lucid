beb9efe6045111096662a58d774d09a75fb08046
Tom <49213475+ProbTom@users.noreply.github.com>
2024-12-19 00:09:21 +0100

    Update config.lua

--- config.lua
+++ config.lua
@@ -6,10 +6,10 @@ local Config = {
         Theme = "Rose"
     },
     URLs = {
-        -- Updated URLs for Fluent UI
-        Fluent = "https://raw.githubusercontent.com/dawid-scripts/Fluent/main/src/Fluent.lua",
-        SaveManager = "https://raw.githubusercontent.com/dawid-scripts/Fluent/main/src/Addons/SaveManager.lua",
-        InterfaceManager = "https://raw.githubusercontent.com/dawid-scripts/Fluent/main/src/Addons/InterfaceManager.lua"
+        -- Correct URLs for Fluent UI
+        Fluent = "https://raw.githubusercontent.com/dawid-scripts/Fluent/master/Main.lua",
+        SaveManager = "https://raw.githubusercontent.com/dawid-scripts/Fluent/master/Addons/SaveManager.lua",
+        InterfaceManager = "https://raw.githubusercontent.com/dawid-scripts/Fluent/master/Addons/InterfaceManager.lua"
     },
     GameID = 16732694052
 }
