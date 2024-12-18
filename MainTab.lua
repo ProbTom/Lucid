e1bd4f2cf62880d92c1faf00f80bb128505968b2
Narcisses <49213475+ovoxos@users.noreply.github.com>
2024-12-18 19:23:04 +0100

    Add files via upload

---
+++ MainTab.lua
@@ -0,0 +1,108 @@
+-- // Main Tab // --
+local section = Tabs.Main:AddSection("Auto Fishing")
+local autoCast = Tabs.Main:AddToggle("autoCast", {Title = "Auto Cast", Default = false })
+autoCast:OnChanged(function()
+    local RodName = ReplicatedStorage.playerstats[LocalPlayer.Name].Stats.rod.Value
+    if Options.autoCast.Value == true then
+        autoCastEnabled = true
+        if LocalPlayer.Backpack:FindFirstChild(RodName) then
+            LocalPlayer.Character.Humanoid:EquipTool(LocalPlayer.Backpack:FindFirstChild(RodName))
+        end
+        if LocalCharacter then
+            local tool = LocalCharacter:FindFirstChildOfClass("Tool")
+            if tool then
+                local hasBobber = tool:FindFirstChild("bobber")
+                if not hasBobber then
+                    if CastMode == "Legit" then
+                        VirtualInputManager:SendMouseButtonEvent(0, 0, 0, true, LocalPlayer, 0)
+                        HumanoidRootPart.ChildAdded:Connect(function()
+                            if HumanoidRootPart:FindFirstChild("power") ~= nil and HumanoidRootPart.power.powerbar.bar ~= nil then
+                                HumanoidRootPart.power.powerbar.bar.Changed:Connect(function(property)
+                                    if property == "Size" then
+                                        if HumanoidRootPart.power.powerbar.bar.Size == UDim2.new(1, 0, 1, 0) then
+                                            VirtualInputManager:SendMouseButtonEvent(0, 0, 0, false, LocalPlayer, 0)
+                                        end
+                                    end
+                                end)
+                            end
+                        end)
+                    elseif CastMode == "Blatant" then
+                        local rod = LocalCharacter and LocalCharacter:FindFirstChildOfClass("Tool")
+                        if rod and rod:FindFirstChild("values") and string.find(rod.Name, "Rod") then
+                            task.wait(0.5)
+                            local Random = math.random(90, 99)
+                            rod.events.cast:FireServer(Random)
+                        end
+                    end
+                end
+            end
+            task.wait(1)
+        end
+    else
+        autoCastEnabled = false
+    end
+end)
+local autoShake = Tabs.Main:AddToggle("autoShake", {Title = "Auto Shake", Default = false })
+autoShake:OnChanged(function()
+    if Options.autoShake.Value == true then
+        autoShakeEnabled = true
+        startAutoShake()
+    else
+        autoShakeEnabled = false
+        stopAutoShake()
+    end
+end)
+local autoReel = Tabs.Main:AddToggle("autoReel", {Title = "Auto Reel", Default = false })
+autoReel:OnChanged(function()
+    if Options.autoReel.Value == true then
+        autoReelEnabled = true
+        startAutoReel()
+    else
+        autoReelEnabled = false
+        stopAutoReel()
+    end
+end)
+local FreezeCharacter = Tabs.Main:AddToggle("FreezeCharacter", {Title = "Freeze Character", Default = false })
+FreezeCharacter:OnChanged(function()
+    local oldpos = HumanoidRootPart.CFrame
+    FreezeChar = Options.FreezeCharacter.Value
+    task.wait()
+    while WaitForSomeone(RenderStepped) do
+        if FreezeChar and HumanoidRootPart ~= nil then
+            task.wait()
+            HumanoidRootPart.CFrame = oldpos
+        else
+            break
+        end
+    end
+end)
+
+-- // Mode Tab // --
+local section = Tabs.Main:AddSection("Mode Fishing")
+local autoCastMode = Tabs.Main:AddDropdown("autoCastMode", {
+    Title = "Auto Cast Mode",
+    Values = {"Legit", "Blatant"},
+    Multi = false,
+    Default = CastMode,
+})
+autoCastMode:OnChanged(function(Value)
+    CastMode = Value
+end)
+local autoShakeMode = Tabs.Main:AddDropdown("autoShakeMode", {
+    Title = "Auto Shake Mode",
+    Values = {"Navigation", "Mouse"},
+    Multi = false,
+    Default = ShakeMode,
+})
+autoShakeMode:OnChanged(function(Value)
+    ShakeMode = Value
+end)
+local autoReelMode = Tabs.Main:AddDropdown("autoReelMode", {
+    Title = "Auto Reel Mode",
+    Values = {"Legit", "Blatant"},
+    Multi = false,
+    Default = ReelMode,
+})
+autoReelMode:OnChanged(function(Value)
+    ReelMode = Value
+end)
