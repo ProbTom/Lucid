11d254ea55cdd5e4b9f92d1c78854e6de9fa0d70
Tom <49213475+ProbTom@users.noreply.github.com>
2024-12-18 23:17:54 +0100

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,14 @@
+return {
+    UI = {
+        MainColor = Color3.fromRGB(38, 38, 38),
+        ButtonColor = Color3.new(220, 125, 255),
+        MinimizeKey = Enum.KeyCode.RightControl,
+        Theme = "Rose"
+    },
+    URLs = {
+        Fluent = "https://github.com/dawid-scripts/Fluent/releases/latest/download/main.lua",
+        SaveManager = "https://raw.githubusercontent.com/dawid-scripts/Fluent/master/Addons/SaveManager.lua",
+        InterfaceManager = "https://raw.githubusercontent.com/dawid-scripts/Fluent/master/Addons/InterfaceManager.lua"
+    },
+    GameID = 16732694052
+}
