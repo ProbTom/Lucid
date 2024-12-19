beeef1f660592f7d52197c8d59c58db98189e5a9
Tom <49213475+ProbTom@users.noreply.github.com>
2024-12-19 12:05:01 +0100

    Update MainTab.lua

--- MainTab.lua
+++ MainTab.lua
@@ -1,5 +1,4 @@
 local RunService = game:GetService("RunService")
-local VirtualUser = game:GetService("VirtualUser")
 local Players = game:GetService("Players")
 local LocalPlayer = Players.LocalPlayer
 
@@ -13,41 +12,42 @@ local mainSection = Tabs.Main:AddSection("Auto Fishing")
 -- Auto Cast Toggle
 local autoCast = Tabs.Main:AddToggle("autoCast", {Title = "Auto Cast", Default = false})
 autoCast:OnChanged(function()
-    Options.autoCast.Value = autoCast.Value
-    if Options.autoCast.Value then
-        if LocalPlayer.Character then
-            Functions.autoCast(CastMode, LocalPlayer.Character, LocalPlayer.Character:FindFirstChild("HumanoidRootPart"))
-        end
+    if autoCast.Value then
+        RunService:BindToRenderStep("AutoCast", 1, function()
+            if LocalPlayer.Character then
+                Functions.autoCast(CastMode, LocalPlayer.Character, LocalPlayer.Character:FindFirstChild("HumanoidRootPart"))
+            end
+        end)
+    else
+        RunService:UnbindFromRenderStep("AutoCast")
     end
 end)
 
 -- Auto Shake Toggle
 local autoShake = Tabs.Main:AddToggle("autoShake", {Title = "Auto Shake", Default = false})
 autoShake:OnChanged(function()
-    Options.autoShake.Value = autoShake.Value
-    if Options.autoShake.Value then
-        if LocalPlayer.PlayerGui then
-            pcall(function()
-                if LocalPlayer.PlayerGui:FindFirstChild("shakeui") and 
-                   LocalPlayer.PlayerGui.shakeui.Enabled then
-                    LocalPlayer.PlayerGui.shakeui.safezone.button.Size = UDim2.new(1001, 0, 1001, 0)
-                    VirtualUser:Button1Down(Vector2.new(1, 1))
-                    task.wait(0.1)
-                    VirtualUser:Button1Up(Vector2.new(1, 1))
-                end
-            end)
-        end
+    if autoShake.Value then
+        RunService:BindToRenderStep("AutoShake", 1, function()
+            if LocalPlayer.PlayerGui then
+                Functions.autoShake(LocalPlayer.PlayerGui)
+            end
+        end)
+    else
+        RunService:UnbindFromRenderStep("AutoShake")
     end
 end)
 
 -- Auto Reel Toggle
 local autoReel = Tabs.Main:AddToggle("autoReel", {Title = "Auto Reel", Default = false})
 autoReel:OnChanged(function()
-    Options.autoReel.Value = autoReel.Value
-    if Options.autoReel.Value then
-        if LocalPlayer and LocalPlayer.PlayerGui then
-            Functions.autoReel(LocalPlayer.PlayerGui, ReelMode)
-        end
+    if autoReel.Value then
+        RunService:BindToRenderStep("AutoReel", 1, function()
+            if LocalPlayer.PlayerGui then
+                Functions.autoReel(LocalPlayer.PlayerGui, ReelMode)
+            end
+        end)
+    else
+        RunService:UnbindFromRenderStep("AutoReel")
     end
 end)
 
@@ -71,27 +71,6 @@ local reelModeDropdown = Tabs.Main:AddDropdown("ReelMode", {
 })
 reelModeDropdown:OnChanged(function(Value)
     ReelMode = Value
-end)
-
--- Set up auto-update loop
-RunService.RenderStepped:Connect(function()
-    if Options.autoCast.Value then
-        if LocalPlayer.Character then
-            Functions.autoCast(CastMode, LocalPlayer.Character, LocalPlayer.Character:FindFirstChild("HumanoidRootPart"))
-        end
-    end
-    
-    if Options.autoShake.Value then
-        if LocalPlayer.PlayerGui then
-            Functions.autoShake(LocalPlayer.PlayerGui)
-        end
-    end
-    
-    if Options.autoReel.Value then
-        if LocalPlayer.PlayerGui then
-            Functions.autoReel(LocalPlayer.PlayerGui, ReelMode)
-        end
-    end
 end)
 
 return true
