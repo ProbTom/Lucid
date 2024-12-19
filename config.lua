4e8cebe9714e8898103e6f0acb47310f35fb87b5
Tom <49213475+ProbTom@users.noreply.github.com>
2024-12-19 01:05:56 +0100

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,15 +1,14 @@
-return {
+getgenv().Config = {
     UI = {
         MainColor = Color3.fromRGB(38, 38, 38),
         ButtonColor = Color3.new(220, 125, 255),
         MinimizeKey = Enum.KeyCode.RightControl,
         Theme = "Rose"
     },
+    GameID = 16732694052,
     URLs = {
-        -- These are the working URLs from your original script
         Fluent = "https://github.com/dawid-scripts/Fluent/releases/latest/download/main.lua",
         SaveManager = "https://raw.githubusercontent.com/dawid-scripts/Fluent/master/Addons/SaveManager.lua",
         InterfaceManager = "https://raw.githubusercontent.com/dawid-scripts/Fluent/master/Addons/InterfaceManager.lua"
-    },
-    GameID = 16732694052
+    }
 }
