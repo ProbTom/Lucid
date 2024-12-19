e0c5be013e26fec8e861dacc8803c4ccf2e6411f
Tom <49213475+ProbTom@users.noreply.github.com>
2024-12-19 12:33:48 +0100

    Update MainTab.lua

--- MainTab.lua
+++ MainTab.lua
@@ -3,8 +3,14 @@ local Players = game:GetService("Players")
 local LocalPlayer = Players.LocalPlayer
 
 -- Initialize state variables
-local CastMode = "Legit"
-local ReelMode = "Blatant"
+local CastMode = getgenv().Options.CastMode.Value or "Legit"
+local ReelMode = getgenv().Options.ReelMode.Value or "Blatant"
+
+-- Ensure Functions table exists
+if not getgenv().Functions then
+    error("Functions table not loaded!")
+    return
+end
 
 -- Main Section
 local mainSection = Tabs.Main:AddSection("Auto Fishing")
@@ -12,43 +18,49 @@ local mainSection = Tabs.Main:AddSection("Auto Fishing")
 -- Auto Cast Toggle
 local autoCast = Tabs.Main:AddToggle("autoCast", {Title = "Auto Cast", Default = false})
 autoCast:OnChanged(function()
-    if autoCast.Value then
-        RunService:BindToRenderStep("AutoCast", 1, function()
-            if LocalPlayer.Character then
-                Functions.autoCast(CastMode, LocalPlayer.Character, LocalPlayer.Character:FindFirstChild("HumanoidRootPart"))
-            end
-        end)
-    else
-        RunService:UnbindFromRenderStep("AutoCast")
-    end
+    pcall(function()
+        if autoCast.Value then
+            RunService:BindToRenderStep("AutoCast", 1, function()
+                if LocalPlayer.Character then
+                    Functions.autoCast(CastMode, LocalPlayer.Character, LocalPlayer.Character:FindFirstChild("HumanoidRootPart"))
+                end
+            end)
+        else
+            RunService:UnbindFromRenderStep("AutoCast")
+        end
+    end)
 end)
 
 -- Auto Shake Toggle
 local autoShake = Tabs.Main:AddToggle("autoShake", {Title = "Auto Shake", Default = false})
 autoShake:OnChanged(function()
-    if autoShake.Value then
-        RunService:BindToRenderStep("AutoShake", 1, function()
-            if LocalPlayer.PlayerGui then
-                Functions.autoShake(LocalPlayer.PlayerGui)
-            end
-        end)
-    else
-        RunService:UnbindFromRenderStep("AutoShake")
-    end
+    pcall(function()
+        if autoShake.Value then
+            RunService:BindToRenderStep("AutoShake", 1, function()
+                if LocalPlayer.PlayerGui then
+                    Functions.autoShake(LocalPlayer.PlayerGui)
+                end
+            end)
+        else
+            RunService:UnbindFromRenderStep("AutoShake")
+        end
+    end)
 end)
 
 -- Auto Reel Toggle
 local autoReel = Tabs.Main:AddToggle("autoReel", {Title = "Auto Reel", Default = false})
 autoReel:OnChanged(function()
-    if autoReel.Value then
-        RunService:BindToRenderStep("AutoReel", 1, function()
-            if LocalPlayer.PlayerGui then
-                Functions.autoReel(LocalPlayer.PlayerGui, ReelMode)
-            end
-        end)
-    else
-        RunService:UnbindFromRenderStep("AutoReel")
-    end
+    pcall(function()
+        if autoReel.Value then
+            RunService:BindToRenderStep("AutoReel", 1, function()
+                if LocalPlayer.PlayerGui then
+                    Functions.autoReel(LocalPlayer.PlayerGui, ReelMode)
+                end
+            end)
+        else
+            RunService:UnbindFromRenderStep("AutoReel")
+        end
+    end)
 end)
 
 -- Cast Mode Dropdown
@@ -58,8 +70,10 @@ local castModeDropdown = Tabs.Main:AddDropdown("CastMode", {
     Multi = false,
     Default = CastMode
 })
+
 castModeDropdown:OnChanged(function(Value)
     CastMode = Value
+    getgenv().Options.CastMode.Value = Value
 end)
 
 -- Reel Mode Dropdown
@@ -69,8 +83,37 @@ local reelModeDropdown = Tabs.Main:AddDropdown("ReelMode", {
     Multi = false,
     Default = ReelMode
 })
+
 reelModeDropdown:OnChanged(function(Value)
     ReelMode = Value
+    getgenv().Options.ReelMode.Value = Value
 end)
+
+-- Cleanup handler
+local function cleanupTab()
+    pcall(function()
+        RunService:UnbindFromRenderStep("AutoCast")
+        RunService:UnbindFromRenderStep("AutoShake")
+        RunService:UnbindFromRenderStep("AutoReel")
+        
+        -- Reset toggles
+        autoCast:SetValue(false)
+        autoShake:SetValue(false)
+        autoReel:SetValue(false)
+        
+        -- Save current modes
+        getgenv().Options.CastMode.Value = CastMode
+        getgenv().Options.ReelMode.Value = ReelMode
+    end)
+end
+
+-- Add cleanup to global cleanup function
+if getgenv().cleanup then
+    local oldCleanup = getgenv().cleanup
+    getgenv().cleanup = function()
+        cleanupTab()
+        oldCleanup()
+    end
+end
 
 return true
