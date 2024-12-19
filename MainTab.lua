661580339e12cdbaedfe38a92572f8e22d9f199e
Tom <49213475+ProbTom@users.noreply.github.com>
2024-12-19 14:55:38 +0100

    Update MainTab.lua

--- MainTab.lua
+++ MainTab.lua
@@ -18,6 +18,7 @@ if not waitForDependency("Options") then return false end
 local RunService = game:GetService("RunService")
 local Players = game:GetService("Players")
 local LocalPlayer = Players.LocalPlayer
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
 
 -- Cache MainTab reference
 local MainTab = getgenv().Tabs.Main
@@ -48,18 +49,39 @@ autoCast:OnChanged(function()
     end)
 end)
 
--- Auto Shake Toggle with fix
+-- Auto Shake Toggle with lure percentage display
 local autoShake = MainTab:AddToggle("autoShake", {
     Title = "Auto Shake",
     Default = false
 })
 
 local lastShakeClick = 0
+local lureGui = nil
+
+local function ExportValue(arg1, arg2)
+    return tonumber(string.format("%."..(arg2 or 1)..'f', arg1))
+end
+
 autoShake:OnChanged(function()
     pcall(function()
         if autoShake.Value then
+            -- Create lure percentage GUI
+            if not lureGui then
+                lureGui = game:GetService("ReplicatedStorage").resources.items.items.GPS.GPS.gpsMain.xyz:Clone()
+                lureGui.Parent = game.Players.LocalPlayer.PlayerGui:WaitForChild("hud"):WaitForChild("safezone"):WaitForChild("backpack")
+                lureGui.Name = "Lure"
+                lureGui.Text = "<font color='#ff4949'>Lure </font>: 0%"
+            end
+
             RunService:BindToRenderStep("AutoShake", 1, function()
                 if LocalPlayer.PlayerGui and tick() - lastShakeClick > 0.1 then
+                    -- Update lure percentage
+                    local rodName = ReplicatedStorage.playerstats[LocalPlayer.Name].Stats.rod.Value
+                    local rod = LocalPlayer.Character and LocalPlayer.Character:FindFirstChild(rodName)
+                    if rod and rod.values.lure then
+                        lureGui.Text = "<font color='#ff4949'>Lure </font>: "..tostring(ExportValue(tostring(rod.values.lure.Value), 2)).."%"
+                    end
+
                     local shakeui = LocalPlayer.PlayerGui:FindFirstChild("shakeui")
                     if shakeui and shakeui.Enabled then
                         local button = shakeui.safezone:FindFirstChild("button")
@@ -75,6 +97,11 @@ autoShake:OnChanged(function()
             end)
         else
             RunService:UnbindFromRenderStep("AutoShake")
+            -- Remove lure GUI when disabled
+            if lureGui then
+                lureGui:Destroy()
+                lureGui = nil
+            end
         end
     end)
 end)
@@ -110,6 +137,12 @@ local function cleanupTab()
         if autoCast then autoCast:SetValue(false) end
         if autoShake then autoShake:SetValue(false) end
         if autoReel then autoReel:SetValue(false) end
+
+        -- Clean up lure GUI if it exists
+        if lureGui then
+            lureGui:Destroy()
+            lureGui = nil
+        end
     end)
 end
 
