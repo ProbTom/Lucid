d032527b6394d43c8e3c4c1b1a67eba47e45e940
Tom <49213475+ProbTom@users.noreply.github.com>
2024-12-19 12:05:28 +0100

    Update Tab.lua

--- Tab.lua
+++ Tab.lua
@@ -1,27 +1,18 @@
 local Window = Fluent:CreateWindow({
     Title = "Lucid Hub",
     TabWidth = 160,
-    Size = UDim2.fromOffset(580, 460),
-    Acrylic = false -- Removed blur effect
+    Size = UDim2.fromOffset(580, 460)
 })
 
--- Create basic tabs
 local Tabs = {
-    Main = Window:AddTab({ 
-        Title = "Main", 
-        Icon = "fishing-rod"
-    }),
-    Items = Window:AddTab({ 
-        Title = "Items", 
-        Icon = "box" 
-    }),
-    Teleports = Window:AddTab({ 
-        Title = "Teleports", 
-        Icon = "map-pin" 
-    })
+    Main = Window:AddTab({ Title = "Main", Icon = "rod" }),
+    Items = Window:AddTab({ Title = "Items", Icon = "box" }),
+    Teleports = Window:AddTab({ Title = "Teleports", Icon = "map" }),
+    Fishing = Window:AddTab({ Title = "Fishing", Icon = "fish" }),
+    Pets = Window:AddTab({ Title = "Pets", Icon = "paw-print" }),
+    Eggs = Window:AddTab({ Title = "Eggs", Icon = "egg" }),
+    Misc = Window:AddTab({ Title = "Misc", Icon = "settings" })
 }
 
--- Export tabs for other modules
 getgenv().Tabs = Tabs
-
 return true
