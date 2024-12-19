58fa439685e4a4501df8dbcccafc3f1158efbd8c
Tom <49213475+ProbTom@users.noreply.github.com>
2024-12-19 16:36:52 +0100

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,3 +1,4 @@
+-- config.lua
 getgenv().Config = {
     UI = {
         MainColor = Color3.fromRGB(38, 38, 38),
@@ -7,12 +8,72 @@ getgenv().Config = {
     },
     GameID = 16732694052,
     URLs = {
-        -- Updated URLs to ensure compatibility
         Fluent = "https://github.com/dawid-scripts/Fluent/releases/latest/download/main.lua",
         SaveManager = "https://raw.githubusercontent.com/dawid-scripts/Fluent/master/Addons/SaveManager.lua",
         InterfaceManager = "https://raw.githubusercontent.com/dawid-scripts/Fluent/master/Addons/InterfaceManager.lua"
     },
-    Debug = true -- Add debug flag for better error handling
+    Items = {
+        ChestRange = {
+            Default = 50,
+            Min = 10,
+            Max = 100
+        },
+        RodRanking = {
+            "Rod Of The Forgotten Fang",
+            "Rod Of The Eternal King",
+            "Rod Of The Depth",
+            "No-Life Rod,
+            "Krampus's Rod",
+            "Trident Rod",
+            "Kings Rod",
+            "Aurora Rod",
+            "Mythical Rod",
+            "Destiny Rod",
+            "Celestial Rod",
+            "Voyager Rod",
+            "Riptide Rod",
+            "Seasons Rod",
+            "Resourceful Rod",
+            "Precision Rod",
+            "Steady Rod",
+            "Nocturnal Rod",
+            "Reinforced Rod",
+            "Magnet Rod",
+            "Rapid Rod",
+            "Fortune Rod",
+            "Phoenix Rod",
+            "Scurvy Rod",
+            "Midas Rod",
+            "Buddy Bond Rod",
+            "Haunted Rod",
+            "Relic Rod",
+            "Antler Rod",
+            "North-Star Rod",
+            "Astral Rod",
+            "Event Horizon Rod",
+            "Candy Cane Rod",
+            "Fungal Rod",
+            "Magma Rod",
+            "Long Rod",
+            "Lucky Rod",
+            "Fast Rod",
+            "Stone Rod",
+            "Carbon Rod",
+            "Plastic Rod",
+            "Training Rod",
+            "Fischer's Rod",
+            "Flismy Rod"
+        },
+        FishRarities = {
+            "Common",
+            "Uncommon",
+            "Rare",
+            "Epic",
+            "Legendary",
+            "Mythical"
+        }
+    },
+    Debug = true
 }
 
 return true
