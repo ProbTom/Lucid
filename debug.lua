f850e87c32a6c91bd112804ea3c1e87ebf8d1719
Tom <49213475+ProbTom@users.noreply.github.com>
2024-12-20 14:55:08 +0100

    Update debug.lua

--- debug.lua
+++ debug.lua
@@ -1,18 +1,16 @@
 -- debug.lua
-local Debug = {
-    _enabled = true
-}
+local Debug = {}
 
 function Debug.Log(message)
-    if Debug._enabled then
-        print(string.format("[%s] %s", os.date("%H:%M:%S"), message))
-    end
+    print("[LOG]:", message)
 end
 
 function Debug.Error(message)
-    if Debug._enabled then
-        warn(string.format("[%s] [ERROR] %s", os.date("%H:%M:%S"), message))
-    end
+    warn("[ERROR]:", message)
 end
 
-function
+function Debug.Warning(message)
+    warn("[WARNING]:", message)
+end
+
+return Debug
