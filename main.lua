25ecbba24f732ab7e1c2de32d341a43fd24655a6
Tom <49213475+ProbTom@users.noreply.github.com>
2024-12-20 14:54:29 +0100

    Update main.lua

--- main.lua
+++ main.lua
@@ -3,13 +3,38 @@ if not game:IsLoaded() then
     game.Loaded:Wait()
 end
 
-local success, result = pcall(function()
-    return loadstring(game:HttpGet("https://raw.githubusercontent.com/ProbTom/Lucid/main/loader.lua"))()
-end)
+local function start()
+    -- Load loader
+    local success, loader = pcall(function()
+        return loadstring(game:HttpGet("https://raw.githubusercontent.com/ProbTom/Lucid/main/loader.lua"))()
+    end)
 
-if not success then
-    warn("Failed to load Lucid Hub:", result)
-    return
+    if not success then
+        warn("[ERROR]: Failed to load loader:", loader)
+        return false
+    end
+
+    -- Initialize
+    if loader and type(loader.Initialize) == "function" then
+        success = pcall(function()
+            return loader.Initialize()
+        end)
+
+        if not success then
+            warn("[ERROR]: Failed to initialize")
+            return false
+        end
+    else
+        warn("[ERROR]: Invalid loader")
+        return false
+    end
+
+    return true
 end
 
-return true
+local success = start()
+if not success then
+    warn("[ERROR]: Script failed to start")
+end
+
+return success
